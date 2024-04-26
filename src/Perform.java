07e22f0e421fc5ce7879730b6e2d3afaad936dbd
munginisfree <s01099684205@gmail.com>
2024-04-26 14:31:49 +0900

    #performRepo, PerformView setup

--- src/Perform.java
+++ src/Perform.java
@@ -1,17 +1,23 @@
 package src;
+import java.util.*;
 
-import java.util.Objects;
-
+// 제목, 장소, 관람시간, 공연시간, 가격, 연령(등급), 좌석등급
 public class Perform {
     private String title;
+    private String place;
     private String date;
+    private int runningTime;
     private int price;
+    private int age;
     private String section;
 
-    public Perform(String title, String date, int price, String section) {
+    public Perform(String title, String place, String date, int runningTime, int price, int age, String section) {
         this.title = title;
+        this.place = place;
         this.date = date;
+        this.runningTime = runningTime;
         this.price = price;
+        this.age = age;
         this.section = section;
     }
 
@@ -23,6 +29,14 @@ public class Perform {
         this.title = title;
     }
 
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
     public String getDate() {
         return date;
     }
@@ -31,12 +45,28 @@ public class Perform {
         this.date = date;
     }
 
+    public int getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(int runningTime) {
+        this.runningTime = runningTime;
+    }
+
     public int getPrice() {
         return price;
     }
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getSection() {
@@ -48,11 +78,14 @@ public class Perform {
     }
 
     @Override
-    public String toString() {
-        return "PerformRepository{" +
+    public java.lang.String toString() {
+        return "Perform{" +
                 "title='" + title + '\'' +
+                ", place='" + place + '\'' +
                 ", date='" + date + '\'' +
+                ", runningTime=" + runningTime +
                 ", price=" + price +
+                ", age=" + age +
                 ", section='" + section + '\'' +
                 '}';
     }
@@ -61,12 +94,13 @@ public class Perform {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Perform that = (Perform) o;
-        return price == that.price && Objects.equals(title, that.title) && Objects.equals(date, that.date) && Objects.equals(section, that.section);
+        Perform perform = (Perform) o;
+        return runningTime == perform.runningTime && price == perform.price && age == perform.age && Objects.equals(title, perform.title) && Objects.equals(place, perform.place) && Objects.equals(date, perform.date) && Objects.equals(section, perform.section);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, date, price, section);
+        return Objects.hash(title, place, date, runningTime, price, age, section);
     }
 }
+
